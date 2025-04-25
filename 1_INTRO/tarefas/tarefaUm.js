fa05fb9fe250fe591ed3c3e6a7bb0ddd6d2a9983
jaime-ac <jaimeantonioca97@gmail.com>
2025-04-24 22:21:35 -0300

    finalizando a introdução do curso, realizando a primeira tarefa

---
+++ 1_INTRO/tarefas/tarefaUm.js
@@ -0,0 +1,8 @@
+//tarefa 1 - criar um programa com node que soma dois números e mostra o resultado no terminal;
+
+const a = 15;
+const b = 20;
+console.log(`A soma de ${a} com ${b} é igual a: ${a + b}`);
+
+//nome do arquivo é: tarefaUm.js
+//para executar basta digitar no terminal: node tarefaUm.js
